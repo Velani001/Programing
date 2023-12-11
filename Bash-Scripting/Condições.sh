8cf3d54d4c5a6e2e43b38535edf661eab4500fc5
Weckeez <47722936+chapeleiro001@users.noreply.github.com>
2023-12-11 13:55:53 -0300

    Condições passadas pelo usuário

--- Bash-Scripting/Condições.sh
+++ Bash-Scripting/Condições.sh
@@ -3,12 +3,20 @@
 #Progama que valida condições passadas pelo usuario
 echo "Qual a cor do semaforo?"
 read cor 
+
 #Condição IF (se).
+#Caso o valor passado pelo usuario for verde, esta mesagem será exibida.
 if ["$cor" == "verde"]
 then
-#Caso o valor passado pelo usuario for verde, esta mesagem será exibida.
     echo "siga em frente"
+
+#Caso o valor passado pelo usuario for verde, esta mesagem será exibida.
+elif ["$cor" == "amarelo"]
+then
+    echo "Aguarde"
+
 else 
-#Caso o valor passado não seja verde, está mensagem sera exibida.
+#Caso o valor passado não seja nenhuma das anteriores, está mensagem sera exibida.
     echo "PARE"
 
+fi #Para finalizar a condição.
