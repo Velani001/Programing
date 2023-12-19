48b80813919cb6cccec043966766ea3e223f3136
Weckeez <47722936+chapeleiro001@users.noreply.github.com>
2023-12-19 13:43:15 -0300

    Update progam.c

--- C/progam.c
+++ C/progam.c
@@ -3,9 +3,16 @@
 
 main(void){
     
-    int porta = 80;
-    char ip [] = "192.168.0.1";
+    int porta;
+    char ip[16];
     float ver = 1.1;
+
+    printf("Digite o ip \n");
+    scanf("%s"&ip);
+
+    printf("Digite a porta \n");
+    scanf("%i", &porta);
+    fgets(ip,10,stdin);
 
     printf("Scan versao: %.1f \n", ver);
     printf("Varrendo Host: %s na porta %i \n", ip, porta);
