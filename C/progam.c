0b554f89eff46cdef814d224e63e61b4fc9c9d24
Weckeez <47722936+chapeleiro001@users.noreply.github.com>
2023-12-19 12:25:19 -0300

    Update progam.c

--- C/progam.c
+++ C/progam.c
@@ -3,7 +3,12 @@
 
 main(void){
     
-    printf("Hello Word\n");
+    int porta = 80;
+    char ip [] = "192.168.0.1";
+    float ver = 1.1;
+
+    printf("Scan versao: %.1f \n", ver);
+    printf("Varrendo Host: %s na porta %i \n", ip, porta);
     return 0;
 
 } 
