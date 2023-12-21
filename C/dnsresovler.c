11a417b507e8f2fa081355d8be8e74cd2e8da424
Weckeez <47722936+chapeleiro001@users.noreply.github.com>
2023-12-21 10:13:16 -0300

    dnsrolver 1.0

--- C/dnsresovler.c
+++ C/dnsresovler.c
@@ -6,7 +6,7 @@ int main(int argc, char *argv[]){
     
     struct hostent *alvo = gethostbyname(argv[1]);
 
-    printf("IP: %s\n",inet_ntoa(*((struct in_addr *)alvo->h_addr)));
+    //printf("IP: %s\n",inet_ntoa(*((struct in_addr *)alvo->h_addr)));
 
     return 0;
 
