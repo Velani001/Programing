207c267a02590f8ecc6e8003e652589bca09c621
Weckeez <47722936+chapeleiro001@users.noreply.github.com>
2024-01-17 11:36:46 -0300

    Create consulta.py

---
+++ Python/consulta.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+import socket
+
+meusocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+meusocket.connect(("44.208.5.36",43))
+meusocket.send("desecsecurity.com"+"\r\r")
+resposta = s.recv(1024)
+print(resposta)
