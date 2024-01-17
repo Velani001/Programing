85aa1a30d5fd91889cde0a784c13b9ead82aa760
Weckeez <47722936+chapeleiro001@users.noreply.github.com>
2024-01-17 11:42:24 -0300

    Update consulta.py

--- Python/consulta.py
+++ Python/consulta.py
@@ -4,6 +4,6 @@ import socket
 
 meusocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 meusocket.connect(("44.208.5.36",43))
-meusocket.send("desecsecurity.com"+"\r\r")
+meusocket.send(b"desecsecurity.com\r\r")
 resposta = s.recv(1024)
 print(resposta)
